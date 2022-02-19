870b936e48b1e612220728a7d14c709af25db2b7
Serzhul <wilshere917@gmail.com>
2022-02-19 19:33:19 +0900

    [Solved] BaekJoon 10844

--- BaekJoon/DynamicProgramming_Part1/10844_쉬운 계단 수.js
+++ BaekJoon/DynamicProgramming_Part1/10844_쉬운 계단 수.js
@@ -1,16 +1,30 @@
 const solution = (input) => {
-    const N = input.toString().trim();
+    const N = Number(input.toString().trim());
 
-    const num = +N;
+    const mod = 1000000000;
 
-    const dp = [0];
+    const dp = Array.from({ length: N + 1 }, () => Array(10));
 
-    dp[1] = 9;
+    dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    dp[2] = [1, 1, 2, 2, 2, 2, 2, 2, 2, 1];
 
-    for (let i = 2; i <= N; i++) {
-        dp[i] = dp[i - 1] + 1 + (9 - i - 1) * 2;
+    for (let i = 3; i <= N; i += 1) {
+        for (let j = 0; j <= 9; j += 1) {
+            if (j === 0) {
+                dp[i][j] = dp[i - 1][j + 1] % mod;
+            } else if (j >= 1 && j <= 8) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+            } else {
+                dp[i][j] = dp[i - 1][j - 1] % mod;
+            }
+        }
     }
 
-    console.log(dp[N]);
+    let sum = 0;
+    for (let i = 0; i <= 9; i += 1) {
+        sum += dp[N][i];
+    }
+
+    console.log(sum % mod);
 };
-solution(`3`);
+solution(`2`);
