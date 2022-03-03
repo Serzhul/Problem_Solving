b6054017ebbad58d865a00a232853b56904dba0a
Serzhul <wilshere917@gmail.com>
2022-03-03 16:47:13 +0900

    Programmers 오픈 채팅방 solved

---
+++ Programmers/Level2/오픈채팅방.js
@@ -0,0 +1,25 @@
+function solution(record) {
+    const answer = [];
+    const map = new Map();
+    for (let i = 0; i < record.length; i += 1) {
+        const [state, uid, name] = record[i].split(' ');
+        if (state === 'Leave') {
+            answer.push([uid, '님이 나갔습니다.']);
+            continue;
+        }
+        if (state === 'Enter') {
+            answer.push([uid, '님이 들어왔습니다.']);
+        }
+        map.set(uid, name);
+    }
+    return answer.map((ele) => map.get(ele[0]) + ele[1]);
+}
+
+solution([
+    'Enter uid1234 Muzi',
+    'Enter uid4567 Prodo',
+    'Leave uid4567',
+    'Enter uid1234 Prodo',
+    'Change uid4567 Muzi',
+    'Change uid1234 ',
+]);
