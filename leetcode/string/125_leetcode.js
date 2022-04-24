7a5f885cb38437aeb877fdaaff7f4f465c3c9f60
Serzhul <wilshere917@gmail.com>
2022-04-24 14:34:15 +0900

    [Leet] 125 valid Palindrome

--- leetcode/string/125_leetcode.js
+++ leetcode/string/125_leetcode.js
@@ -1,9 +1,23 @@
-const isPalindrome = (s) => {
-    const matched = s.toLowerCase().match(/[a-zA-Z0-9]/gi);
+// Solution 1 : Reverse 메서드 사용
 
-    return matched === null
-        ? true
-        : [...matched].join('') === [...matched].reverse().join('');
+// const isPalindrome = (s) => {
+//     const matched = s.toLowerCase().match(/[a-zA-Z0-9]/gi);
+
+//     return matched === null
+//         ? true
+//         : [...matched].join('') === [...matched].reverse().join('');
+// };
+
+// Solution 2: 투 포인터
+
+const isPalindrome = (s) => {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+
+    for (let i = 0, j = s.length - 1; i <= j; i += 1, j -= 1) {
+        if (s.charAt(i) !== s.charAt(j)) return false;
+    }
+
+    return true;
 };
 
 isPalindrome('A man, a plan, a canal: Panama');
