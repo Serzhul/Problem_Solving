dbc09a247329372d90af1d421fd5fa46d5a7fa4f
Serzhul <wilshere917@gmail.com>
2022-12-01 10:00:53 +0900

    feature Stack 코드 작성

---
+++ ZeroBase/Stack/Stack.js
@@ -0,0 +1,63 @@
+import Node from "./Node.js";
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  push(data) {
+    const newNode = new Node(data, null);
+
+    if (this.isEmpty()) {
+      this.top = newNode;
+      this.size += 1;
+      return;
+    }
+
+    newNode.next = this.top;
+    this.top = newNode;
+    this.size += 1;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    this.top = this.top.next;
+    this.size -= 1;
+  }
+
+  print() {
+    let cur = this.top;
+
+    console.log(`크기 : ${this.getSize()}`);
+    while (cur !== null) {
+      console.log(cur.data);
+      //   console.log(cur.data);
+      cur = cur.next;
+    }
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return !this.size;
+  }
+}
+
+const newStack = new Stack();
+
+newStack.push("월");
+newStack.print();
+newStack.push("화");
+newStack.print();
+newStack.push("수");
+newStack.print();
+newStack.pop();
+newStack.print();
+newStack.pop();
+newStack.print();
