35bf37f8977b151c49da551e39bab3c039ab2433
Serzhul <wilshere917@gmail.com>
2022-12-07 18:07:43 +0900

    feature : Quicksort 구현

---
+++ ZeroBase/Algorithm/Sorting/QuickSort/QuickSort.js
@@ -0,0 +1,48 @@
+class QuickSort {
+  sort(data, start, end) {
+    if (start === end) return;
+
+    let left = start;
+    let right = end;
+
+    const pivot = data[left];
+
+    // 오른쪽 값이 pivot보다 작으면 pivot 왼쪽으로, 왼쪽 값이 pivot보다 크면 pivot 오른쪽으로
+    while (left < right) {
+      // 피벗값보다 right 값이 큰 경우 Right가 왼쪽으로
+      while (data[right] >= pivot && right > left) {
+        right -= 1;
+      }
+
+      // 그렇지 않은 경우에는 left 위치에 right 값을 대입
+      data[left] = data[right];
+
+      // 피벗값보다 Left값이 작은 경우에는 left가 오른쪽으로 이동
+      while (data[left] <= pivot && left < right) {
+        left += 1;
+      }
+
+      // 그렇지 않은 경우에는 right 위치에 left 값을 대입
+      data[right] = data[left];
+
+      // left와 right가 만나는 지점이 pivot 값이 들어가는 위치
+      if (left === right) {
+        data[left] = pivot;
+        this.sort(data, start, right - 1);
+        this.sort(data, left + 1, end);
+      }
+    }
+
+    return data;
+  }
+}
+
+const quickSort = new QuickSort();
+
+const arr = [6, 2, 3, 8, 4, 5, 1, 7];
+
+console.log(arr);
+
+const sortedArr = quickSort.sort(arr, 0, arr.length - 1);
+
+console.log(sortedArr);
