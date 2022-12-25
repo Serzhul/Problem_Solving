c33354e7ca2ee1371594594433cd268c71793ba6
Serzhul <wilshere917@gmail.com>
2022-12-25 22:14:40 +0900

    Backspace String Compare 문제 풀이

---
+++ leetcode/Crack Algo/Day4/Backspace String Compare.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// Goal Time Complexity : O(n)
+// Goal Space Complexity : O(1)
+
+// const s = "ab#c";
+// const t = "ad#c";
+
+const s = "ab##";
+const t = "c#d#";
+
+// const s = "a#c";
+// const t = "b";
+
+const backspaceCompare = (s, t) => {
+  const s1 = [];
+  const s2 = [];
+
+  [...s].forEach((char) => {
+    if (char === "#") {
+      s1.pop();
+    } else {
+      s1.push(char);
+    }
+  });
+
+  [...t].forEach((char) => {
+    if (char === "#") {
+      s2.pop();
+    } else {
+      s2.push(char);
+    }
+  });
+
+  console.log(s1, s2);
+  return s1.join("") === s2.join("");
+};
+
+console.log(backspaceCompare(s, t));
