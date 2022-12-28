1242ea990013a10609c3198d706a297bf5bfd4bd
Serzhul <wilshere917@gmail.com>
2022-12-28 20:56:32 +0900

    Minimum Size Subarray Sum 문제 풀이 수정

--- leetcode/Crack Algo/Day5/Minimum Size Subarray Sum.js
+++ leetcode/Crack Algo/Day5/Minimum Size Subarray Sum.js
@@ -14,27 +14,23 @@ const target = 11;
 const nums = [1, 2, 3, 4, 5];
 
 const minSubArrayLen = (target, nums) => {
-  let minLen = Number.MAX_SAFE_INTEGER;
+  let left = 0;
+  let right = 0;
+  let sum = 0;
+  let result = 0;
 
-  for (let left = 0; left < nums.length; left += 1) {
-    let sumOfSubarray = 0;
-    let right = left;
+  while (right < nums.length) {
+    sum += nums[right];
 
-    while (sumOfSubarray < target) {
-      if (nums[right] >= target) {
-        minLen = 1;
-        break;
-      }
-      sumOfSubarray += nums[right];
-      right += 1;
+    while (sum >= target) {
+      const len = right - left + 1;
+      if (result === 0 || len < result) result = len;
+      sum -= nums[left];
+      left += 1;
     }
-
-    if (sumOfSubarray < target && minLen === Number.MAX_SAFE_INTEGER) return 0;
-
-    minLen = Math.min(minLen, right - left + 1);
+    right += 1;
   }
-
-  return minLen;
+  return result;
 };
 
 console.log(minSubArrayLen(target, nums));
