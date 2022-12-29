7e4ec5f6ae8cdc272e6f649e832e1ab7c77cf6dc
Serzhul <wilshere917@gmail.com>
2022-12-29 10:19:43 +0900

    Invert Binary Tree 문제 풀이

---
+++ leetcode/LeetCode 75/Day6/Invert Binary Tree.js
@@ -0,0 +1,27 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+let root = new TreeNode(4);
+root.left = new TreeNode(2);
+root.right = new TreeNode(7);
+root.left.left = new TreeNode(1);
+root.left.right = new TreeNode(3);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(9);
+
+const invertTree = (root) => {
+  if (root === null) return null;
+
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
+
+invertTree(root);
