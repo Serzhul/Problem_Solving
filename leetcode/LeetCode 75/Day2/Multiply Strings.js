d346d6d11011a581ca58b8d34534cb01a68496d8
Serzhul <wilshere917@gmail.com>
2023-01-12 10:52:52 +0900

    feature: Multiply String 문제 풀이 추가

--- leetcode/LeetCode 75/Day2/Multiply Strings.js
+++ leetcode/LeetCode 75/Day2/Multiply Strings.js
@@ -17,15 +17,15 @@ const multiply = function (num1, num2) {
   for (let i = m - 1; i >= 0; i -= 1) {
     for (let j = n - 1; j >= 0; j -= 1) {
       const p1 = i + j;
-      const p2 = i + j + 1;
-      const sum = res[p2] + Number(num1[i]) * Number(num2[j]);
+      const p2 = p1 + 1;
+      const sum = res[p2] + Number(num1[i]) * Number(num2[j]); // 곱해서 나온 값을 누적으로 더해줌
 
-      res[p2] = sum % 10;
-      res[p1] += Math.floor(sum / 10);
+      res[p2] = sum % 10; // 10으로 나눈 나머지
+      res[p1] += Math.floor(sum / 10); // 10을 나눈 몫을 더함
     }
   }
   if (res[0] === 0) res.shift();
   return res.join("");
 };
 
-multiply(num1, num2);
+console.log(multiply(num1, num2));
