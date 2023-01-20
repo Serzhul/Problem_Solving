eee7ef6ba8693709091b2a5efff50466f4652df3
Serzhul <wilshere917@gmail.com>
2023-01-20 09:27:11 +0900

    Convert Binary Number 문제 풀이

---
+++ leetcode/Programming Skills/Day10/Convert Binary Number in a Linked List to Integer.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+const getDecimalValue = (head) => {
+  let binary = "";
+
+  while (head) {
+    binary += head.val;
+    head = head.next;
+  }
+
+  console.log(Number.parseInt(binary, 2));
+};
