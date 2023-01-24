3ad1be4f5e8419078b47e6945394b2ebd61dd6af
Serzhul <wilshere917@gmail.com>
2023-01-24 18:07:25 +0900

    Balanced Binary Tree 문제 풀이

---
+++ leetcode/Programming Skills/Day2/Balanced Binary Tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isBalanced = (root) => {
+  let isHeightBalanced = true;
+
+  const getHeight = (tree, height) => {
+    if (!tree) return 0;
+
+    const leftHeight = getHeight(tree.left, height + 1);
+    const rightHeight = getHeight(tree.right, height + 1);
+
+    if (Math.abs(rightHeight - leftHeight) > 1) isHeightBalanced = false;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+
+  getHeight(root, 0);
+
+  return isHeightBalanced;
+};
+
+isBalanced();
