c6e73623224f9b06963f9a6aa6ac8a5e2b331e0f
Serzhul <wilshere917@gmail.com>
2023-01-27 16:55:53 +0900

    Array of Products 문제 풀이

---
+++ AlgoExpert/Medium/Array of Products.js
@@ -0,0 +1,35 @@
+const array = [5, 1, 4, 2];
+
+function arrayOfProducts(array) {
+  const res = [];
+
+  const len = array.length;
+
+  const leftArr = Array.from({ length: len }, () => 1);
+  const rightArr = Array.from({ length: len }, () => 1);
+
+  let products = 1;
+  for (let i = 0; i < len; i += 1) {
+    const cur = array[i];
+
+    leftArr[i] = products;
+    products *= cur;
+  }
+
+  products = 1;
+
+  for (let i = len - 1; i >= 0; i -= 1) {
+    const cur = array[i];
+
+    rightArr[i] = products;
+    products *= cur;
+  }
+
+  for (let i = 0; i < len; i += 1) {
+    res[i] = leftArr[i] * rightArr[i];
+  }
+
+  return res;
+}
+
+console.log(arrayOfProducts(array));
