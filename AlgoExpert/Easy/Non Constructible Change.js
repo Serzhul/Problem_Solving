01269bb7dd36d3bab734b7f9f0e8d9690f5b2b73
Serzhul <wilshere917@gmail.com>
2023-01-30 12:16:10 +0900

    Non Constructible Change 문제 풀이

---
+++ AlgoExpert/Easy/Non Constructible Change.js
@@ -0,0 +1,16 @@
+const coins = [5, 7, 1, 1, 2, 3, 22];
+
+function nonConstructibleChange(coins) {
+  coins.sort((a, b) => a - b);
+
+  let change = 0;
+
+  for (let i = 0; i < coins.length; i += 1) {
+    if (change + 1 < coins[i]) return change + 1;
+    change += coins[i];
+  }
+
+  return change + 1;
+}
+
+console.log(nonConstructibleChange(coins));
