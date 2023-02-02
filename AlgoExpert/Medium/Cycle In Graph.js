1321001c97d93061684deae4e30bbcfa683a8920
Serzhul <wilshere917@gmail.com>
2023-02-02 15:31:36 +0900

    Cycle In Graph 문제 풀이

---
+++ AlgoExpert/Medium/Cycle In Graph.js
@@ -0,0 +1,29 @@
+const edges = [[1, 3], [2, 3, 4], [0], [], [2, 5], []];
+
+function getHasCycle(nodeList, targetNode) {
+  const queue = [targetNode];
+  const visitedNodes = {};
+
+  while (queue.length) {
+    const curNode = queue.shift();
+    if (nodeList[curNode].includes(targetNode)) return true;
+    visitedNodes[curNode] = true;
+    nodeList[curNode].forEach((edge) => {
+      if (!(edge in visitedNodes)) {
+        queue.push(edge);
+      }
+    });
+  }
+
+  return false;
+}
+
+function cycleInGraph(edges) {
+  for (let i = 0; i < edges.length; i += 1) {
+    const hasCycle = getHasCycle(edges, i);
+    if (hasCycle) return hasCycle;
+  }
+  return false;
+}
+
+console.log(cycleInGraph(edges));
