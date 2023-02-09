9b7b4cbdc281c00e63dbe38368e6ad6fe02a9246
Serzhul <wilshere917@gmail.com>
2023-02-09 14:32:46 +0900

    최소직사각형 문제 풀이

--- Programmers/Level1/최소직사각형.js
+++ Programmers/Level1/최소직사각형.js
@@ -1,21 +1,30 @@
-function solution(sizes) {
-    let maxWidth1 = 0;
-    let maxHeight1 = 0;
-    let maxWidth2 = 0;
-    let maxHeight2 = 0;
+const sizes = [
+  [10, 7],
+  [12, 3],
+  [8, 15],
+  [14, 7],
+  [5, 15],
+];
 
-    for (let i = 0; i < sizes.length; i += 1) {
-        maxWidth1 = Math.max(sizes[i][0], maxWidth1);
-        maxHeight1 = Math.max(sizes[i][1], maxHeight1);
-        maxWidth2 = Math.max(sizes[i][1], maxWidth2);
-        maxHeight2 = Math.max(sizes[i][0], maxHeight2);
+function solution(sizes) {
+  let maxHorizon = Number.MIN_SAFE_INTEGER;
+  let maxVertical = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < sizes.length; i += 1) {
+    const [horizon, vertical] = sizes[i];
+    const bigger = Math.max(horizon, vertical);
+    const smaller = Math.min(horizon, vertical);
+
+    if (maxHorizon < bigger) {
+      maxHorizon = bigger;
     }
 
-    console.log(maxWidth1, maxWidth2, maxHeight1, maxHeight2);
+    if (maxVertical < smaller) {
+      maxVertical = smaller;
+    }
+  }
+
+  return maxHorizon * maxVertical;
 }
-solution([
-    [60, 50],
-    [30, 70],
-    [60, 30],
-    [80, 40],
-]);
+
+console.log(solution(sizes));
