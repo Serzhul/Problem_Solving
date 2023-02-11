5eefd4c6980a4a1048df5f3e9eb7e47307db8acd
Serzhul <wilshere917@gmail.com>
2023-02-11 16:35:38 +0900

    개인정보 수집 유효기간 풀이

---
+++ Programmers/Level1/개인정보 수집 유효기간.js
@@ -0,0 +1,67 @@
+const today = "2022.05.19";
+const terms = ["A 6", "B 12", "C 3"];
+const privacies = [
+  "2021.05.02 A",
+  "2021.07.01 B",
+  "2022.02.19 C",
+  "2022.02.20 C",
+];
+
+function solution(today, terms, privacies) {
+  const res = [];
+  const [todayYear, todayMonth, todayDay] = today
+    .split(".")
+    .map((date) => Number(date));
+
+  const termsMap = {};
+
+  terms.forEach((term) => {
+    const [termType, validation] = term.split(" ");
+    termsMap[termType] = Number(validation);
+  });
+
+  for (let i = 0; i < privacies.length; i += 1) {
+    const [privacy, type] = privacies[i].split(" ");
+    const [privacyYear, privacyMonth, privacyDay] = privacy
+      .split(".")
+      .map((el) => Number(el));
+
+    let [validateYear, validateMonth, validateDay] = [0, 0, privacyDay];
+
+    const canSaveMonth = termsMap[type];
+
+    validateMonth = privacyMonth + canSaveMonth;
+
+    let yearCount = 0;
+
+    while (validateMonth > 12) {
+      validateMonth -= 12;
+      yearCount += 1;
+    }
+
+    validateYear = privacyYear + yearCount;
+
+    if (validateMonth > 12) {
+      validateYear += 1;
+      validateMonth -= 12;
+    }
+
+    if (privacyDay === 1) {
+      validateMonth -= 1;
+      validateDay = 28;
+    } else validateDay = privacyDay - 1;
+
+    if (todayYear > validateYear) {
+      res.push(i + 1);
+    } else if (todayYear === validateYear) {
+      if (todayMonth > validateMonth) res.push(i + 1);
+      else if (todayMonth === validateMonth) {
+        if (todayDay > validateDay) res.push(i + 1);
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(solution(today, terms, privacies));
