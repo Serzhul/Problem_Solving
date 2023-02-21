5a1e1b9adc78aacaa3b662af90de10216b599c8f
Serzhul <wilshere917@gmail.com>
2023-02-21 11:10:42 +0900

    후보키 풀이

--- Programmers/Level2/후보키.js
+++ Programmers/Level2/후보키.js
@@ -1,18 +1,77 @@
-function solution(relation) {
-    let answer = 0;
+const relation = [
+  ["100", "ryan", "music", "2"],
+  ["200", "apeach", "math", "2"],
+  ["300", "tube", "computer", "3"],
+  ["400", "con", "computer", "4"],
+  ["500", "muzi", "music", "3"],
+  ["600", "apeach", "music", "2"],
+];
 
-    const comb = (list, arr, idx, n, r, target) {
-        if(r === 0) {
-            list.push(Object.assign([], arr))
-        } else if ()
+const getCombinations = (array) => {
+  const combArr = [];
+
+  const getComb = (arr, pick, prefix = []) => {
+    if (pick === 0) {
+      combArr.push([...prefix]);
+      return null;
     }
+
+    return arr.flatMap((v, i) =>
+      getComb(arr.slice(i + 1), pick - 1, [...prefix, v])
+    );
+  };
+
+  for (let i = 1; i <= array.length; i += 1) {
+    getComb(array, i);
+  }
+
+  return combArr;
+};
+
+const checkUniques = (relation, combinations) => {
+  const results = [];
+
+  combinations.forEach((combination) => {
+    const set = new Set();
+
+    relation.forEach((rel) => {
+      set.add(combination.map((comb) => rel[comb]).join(","));
+    });
+
+    if (set.size === relation.length) results.push(combination);
+  });
+
+  return results;
+};
+
+function checkMinimality(combinations) {
+  const results = [];
+
+  while (combinations.length) {
+    results.push(combinations[0]);
+    combinations = combinations.reduce((acc, cur) => {
+      console.log(combinations[0], cur);
+      const notMinimal = combinations[0].every((combination) =>
+        cur.includes(combination)
+      );
+
+      if (!notMinimal) acc.push(cur);
+
+      return acc;
+    }, []);
+  }
+
+  return results;
 }
 
-solution([
-    ['100', 'ryan', 'music', '2'],
-    ['200', 'apeach', 'math', '2'],
-    ['300', 'tube', 'computer', '3'],
-    ['400', 'con', 'computer', '4'],
-    ['500', 'muzi', 'music', '3'],
-    ['600', 'apeach', 'music', '2'],
-]);
+function solution(relation) {
+  const idxArr = Array.from({ length: relation[0].length }, (_, i) => i);
+
+  const combinations = getCombinations(idxArr);
+  const uniqueRelations = checkUniques(relation, combinations);
+  const miniMals = checkMinimality(uniqueRelations);
+
+  return miniMals.length;
+}
+
+console.log(solution(relation));
