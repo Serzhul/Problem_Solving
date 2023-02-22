a3ae62796086802cef49f0ed1a4063c743267db0
Serzhul <wilshere917@gmail.com>
2023-02-22 16:30:50 +0900

    호텔 대실 복습 1회차

--- Programmers/Level2/호텔 대실.js
+++ Programmers/Level2/호텔 대실.js
@@ -1,28 +1,58 @@
 const book_time = [
-  ["15:55", "19:53"],
-  ["15:55", "18:20"],
+  ["15:00", "17:00"],
+  ["16:40", "18:20"],
+  ["14:20", "15:20"],
+  ["14:10", "19:20"],
+  ["18:20", "21:20"],
 ];
 
+// function solution(book_time) {
+//   function getNextTime(string) {
+//     const [exitHour, exitMin] = string.split(":");
+
+//     return Number(exitHour) * 60 + Number(exitMin) + 10;
+//   }
+
+//   book_time.sort();
+
+//   const rooms = [];
+
+//   book_time.forEach((time) => {
+//     const [enterHour, enterMin] = time[0].split(":");
+//     const startTime = Number(enterHour) * 60 + Number(enterMin);
+
+//     const idx = rooms.findIndex((el) => el <= startTime);
+
+//     if (idx === -1) rooms.push(getNextTime(time[1]));
+//     else rooms[idx] = getNextTime(time[1]);
+//   });
+
+//   return rooms.length;
+// }
+
+/* Review 1 */
+const getExitTime = (string) => {
+  const [exitHour, exitMin] = string.split(":").map(Number);
+
+  return exitHour * 60 + exitMin + 10;
+};
+
 function solution(book_time) {
-  function getNextTime(string) {
-    const [exitHour, exitMin] = string.split(":");
-
-    return Number(exitHour) * 60 + Number(exitMin) + 10;
-  }
-
   book_time.sort();
 
   const rooms = [];
 
-  book_time.forEach((time) => {
-    const [enterHour, enterMin] = time[0].split(":");
-    const startTime = Number(enterHour) * 60 + Number(enterMin);
+  for (let i = 0; i < book_time.length; i += 1) {
+    const [enterTime, exitTime] = book_time[i];
+    const [enterHour, enterMin] = enterTime.split(":").map(Number);
+
+    const startTime = enterHour * 60 + enterMin;
 
     const idx = rooms.findIndex((el) => el <= startTime);
 
-    if (idx === -1) rooms.push(getNextTime(time[1]));
-    else rooms[idx] = getNextTime(time[1]);
-  });
+    if (idx === -1) rooms.push(getExitTime(exitTime));
+    else rooms[idx] = getExitTime(exitTime);
+  }
 
   return rooms.length;
 }
