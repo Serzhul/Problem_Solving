259f308945032b1a01a33a6e681d58410743d1e3
Serzhul <wilshere917@gmail.com>
2023-02-28 11:44:59 +0900

    둘만의 암호 복습

--- Programmers/Level1/둘만의 암호.js
+++ Programmers/Level1/둘만의 암호.js
@@ -1,45 +1,74 @@
-// const s = "abccc";
-// const skip = "c";
-// const index = 3;
+const s = "aukks";
+const skip = "wbqd";
+const index = 5;
 
-const s = "ybcde";
-const skip = "az";
-const index = 1;
+// const s = "ybcde";
+// const skip = "az";
+// const index = 1;
 
 // 틀린 풀이
+// function solution(s, skip, index) {
+//   const res = [];
+//   const skipMap = {};
+//   [...skip].forEach((char) => {
+//     skipMap[char] = char.charCodeAt();
+//   });
+
+//   for (let i = 0; i < s.length; i += 1) {
+//     const cur = s[i];
+//     const curCode = cur.charCodeAt();
+//     let skipCount = 0;
+//     let idxCount = 1;
+
+//     while (idxCount <= index) {
+//       const checkChar = String.fromCharCode(curCode + idxCount);
+
+//       if (skipMap[checkChar]) skipCount += 1;
+
+//       idxCount += 1;
+//     }
+
+//     let newCode = curCode + (index + skipCount);
+
+//     if (newCode >= 123) newCode -= 26;
+
+//     while (skipMap[String.fromCharCode(newCode)]) {
+//       newCode += 1;
+//     }
+
+//     res.push(String.fromCharCode(newCode));
+//   }
+
+//   return res.join("");
+// }
+
+// 바른 풀이 => 바로 z가 들어올 경우 고려
+
+const getCharCode = (char) => char.charCodeAt(0);
+
 function solution(s, skip, index) {
-  const res = [];
+  const splitted = s.split("");
   const skipMap = {};
+
   [...skip].forEach((char) => {
-    skipMap[char] = char.charCodeAt();
+    skipMap[char.charCodeAt()] = 1;
   });
 
-  for (let i = 0; i < s.length; i += 1) {
-    const cur = s[i];
-    const curCode = cur.charCodeAt();
-    let skipCount = 0;
-    let idxCount = 1;
+  return splitted
+    .map((char) => {
+      let charCode = getCharCode(char);
+      let count = index;
 
-    while (idxCount <= index) {
-      const checkChar = String.fromCharCode(curCode + idxCount);
+      for (let i = 0; i < count; i += 1) {
+        if (charCode === 122) charCode -= 26;
 
-      if (skipMap[checkChar]) skipCount += 1;
+        charCode += 1;
 
-      idxCount += 1;
-    }
+        if (skipMap[charCode]) count += 1;
+      }
 
-    let newCode = curCode + (index + skipCount);
-
-    if (newCode >= 123) newCode -= 26;
-
-    while (skipMap[String.fromCharCode(newCode)]) {
-      newCode += 1;
-    }
-
-    res.push(String.fromCharCode(newCode));
-  }
-
-  return res.join("");
+      return String.fromCharCode(charCode);
+    })
+    .join("");
 }
-
 console.log(solution(s, skip, index));
