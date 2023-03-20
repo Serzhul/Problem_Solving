5cdff5968c54c035bfbdf6af79cd5d7eeba1667c
Serzhul <wilshere917@gmail.com>
2023-03-20 09:28:40 +0900

    백준 16953 BFS 솔루션  추가

--- Fastcampus/Silver/Silver II/16953_A->B.js
+++ Fastcampus/Silver/Silver II/16953_A->B.js
@@ -1,3 +1,5 @@
+// Solution 1
+
 // const solution = (input) => {
 //   let [A, B] = input.toString().trim().split(" ").map(Number);
 
@@ -28,34 +30,67 @@
 //   else console.log(ans);
 // };
 
-// 다른 솔루션 : 로직 동일, 코드 스타일 다름
+// Solution2 : 로직 동일, 코드 스타일 다름
 
+// const solution = (input) => {
+//   let [A, B] = input.toString().trim().split(" ").map(Number);
+
+//   /*
+//   B를 최대한 A로 나눈다.
+//   더 나눌 수 없는 경우 1을 뺀다.
+//   그 값을 또 A로 최대한 나눈다.
+//   0이 될 때까지 반복한다. (만약 0이 안되면 -1을 반환한다)
+//   */
+
+//   let ans = 1;
+//   let flag = false;
+
+//   while (A <= B) {
+//     if (A === B) {
+//       flag = true;
+//       break;
+//     }
+//     if (B % 2 === 0) B = parseInt(B / 2);
+//     else if (B % 10 === 1) B = parseInt(B / 10);
+//     else break;
+//     ans += 1;
+//   }
+
+//   if (flag) console.log(ans);
+//   else console.log(-1);
+// };
+
+// Solution 3 : BFS
 const solution = (input) => {
   let [A, B] = input.toString().trim().split(" ").map(Number);
 
-  /*
-  B를 최대한 A로 나눈다.
-  더 나눌 수 없는 경우 1을 뺀다.
-  그 값을 또 A로 최대한 나눈다.
-  0이 될 때까지 반복한다. (만약 0이 안되면 -1을 반환한다)
-  */
+  const queue = [];
 
-  let ans = 1;
-  let flag = false;
+  const checked = {};
 
-  while (A <= B) {
-    if (A === B) {
-      flag = true;
+  queue.push([A * 2, 0]);
+  queue.push([Number(`${A}1`), 0]);
+
+  while (queue.length) {
+    const [cur, time] = queue.shift();
+
+    checked[cur] = time + 1;
+
+    if (cur === B) {
       break;
     }
-    if (B % 2 === 0) B = parseInt(B / 2);
-    else if (B % 10 === 1) B = parseInt(B / 10);
-    else break;
-    ans += 1;
+
+    if (!checked[cur * 2] && cur * 2 <= B) {
+      queue.push([cur * 2, time + 1]);
+    }
+
+    if (!checked[Number(`${cur}1`)] && Number(`${cur}1`) <= B) {
+      queue.push([Number(`${cur}1`), time + 1]);
+    }
   }
 
-  if (flag) console.log(ans);
-  else console.log(-1);
+  if (!checked[B]) console.log(-1);
+  else console.log(checked[B] + 1);
 };
 
-solution(`100 40021`);
+solution(`2 162`);
