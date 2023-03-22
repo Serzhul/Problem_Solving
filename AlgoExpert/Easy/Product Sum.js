1285416624a7f81bdd5cc7fa1b90e7bdba4b6416
Serzhul <wilshere917@gmail.com>
2023-03-22 10:56:03 +0900

    Product Sum 복습

--- AlgoExpert/Easy/Product Sum.js
+++ AlgoExpert/Easy/Product Sum.js
@@ -1,5 +1,21 @@
 const array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]];
 
+// function productSum(array, depth = 1) {
+//   let sum = 0;
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     const cur = array[i];
+
+//     if (typeof cur === "number") {
+//       sum += cur;
+//     } else {
+//       sum += productSum(cur, depth + 1);
+//     }
+//   }
+
+//   return sum * depth;
+// }
+
 function productSum(array, depth = 1) {
   let sum = 0;
 
