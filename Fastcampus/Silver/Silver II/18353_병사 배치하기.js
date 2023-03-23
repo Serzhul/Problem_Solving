c81d364bc85065caba7d8ff86439efab03bfa76a
Serzhul <wilshere917@gmail.com>
2023-03-23 10:11:52 +0900

    DP 문제 4 풀이

--- Fastcampus/Silver/Silver II/18353_병사 배치하기.js
+++ Fastcampus/Silver/Silver II/18353_병사 배치하기.js
@@ -11,6 +11,35 @@ function lowerBound(arr, target) {
   return end;
 }
 
+// Solution 1 : Parametric Search
+
+// const solution = (input) => {
+//   const [N, soldiers] = input
+//     .toString()
+//     .trim()
+//     .split("\n")
+//     .map((el, idx) => {
+//       if (idx === 0) return Number(el);
+//       return el.split(" ").map(Number);
+//     });
+
+//   soldiers.reverse(); // 순서를 뒤집어서 최장 증가 부분 수열 문제로 변환
+
+//   const arr = [0];
+//   for (let i = 0; i < soldiers.length; i += 1) {
+//     const soldier = soldiers[i];
+//     if (arr[arr.length - 1] < soldier) arr.push(soldier);
+//     else {
+//       const idx = lowerBound(arr, soldier);
+//       arr[idx] = soldier;
+//     }
+//   }
+
+//   console.log(N - (arr.length - 1));
+// };
+
+// Solution ２ : Dynamic Programming
+
 const solution = (input) => {
   const [N, soldiers] = input
     .toString()
@@ -21,19 +50,19 @@ const solution = (input) => {
       return el.split(" ").map(Number);
     });
 
-  soldiers.reverse(); // 순서를 뒤집어서 최장 증가 부분 수열 문제로 변환
+  const dp = Array.from({ length: N }, () => 1);
 
-  const arr = [0];
-  for (let i = 0; i < soldiers.length; i += 1) {
-    const soldier = soldiers[i];
-    if (arr[arr.length - 1] < soldier) arr.push(soldier);
-    else {
-      const idx = lowerBound(arr, soldier);
-      arr[idx] = soldier;
+  soldiers.reverse();
+
+  for (let i = 1; i < N; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (soldiers[j] < soldiers[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
     }
   }
 
-  console.log(N - (arr.length - 1));
+  console.log(dp);
+
+  console.log(N - Math.max(...dp));
 };
 
 solution(`7
---
+++ Fastcampus/Silver/Silver III/1463_1로 만들기.js
@@ -0,0 +1,20 @@
+const solution = (input) => {
+  const [N] = input.toString().trim().split("\n").map(Number);
+
+  const dp = Array(N + 1).fill(0);
+
+  for (let x = 2; x <= N; x += 1) {
+    dp[x] = dp[x - 1];
+    if (x % 2 === 0) {
+      dp[x] = Math.min(dp[x], dp[parseInt(x / 2, 10)]);
+    }
+    if (x % 3 === 0) {
+      dp[x] = Math.min(dp[x], dp[parseInt(x / 3, 10)]);
+    }
+    dp[x] += 1;
+  }
+
+  console.log(dp[N]);
+};
+
+solution(`10`);
